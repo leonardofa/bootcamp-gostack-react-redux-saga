6df34b907ab8723f3759c6a2fba68155c4301d69
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-02-27 22:27:16 -0300

    Estilos globais

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import GloblaStyle from './styles/global';
 
 function App() {
   return (
     <BrowserRouter>
       {/* <Header /> */}
       <Routes />
+      <GloblaStyle />
     </BrowserRouter>
   );
 }
